0153444b94374647281bc6b427c60b57c6d2a55d
Nishchal Gautam <cyberhck@users.noreply.github.com>
2022-04-22 14:03:58 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "service_account_key_private_keys" {
   sensitive = true
   value     = module.google_service_account_instance.service_account_keys
 }
-
-output "service_account_name" {
-  value       = google_service_account.service_account.name
-  sensitive   = false
-  description = "Theh name of `google_service_account` resource."
-}
