dd5499e09bb21976b4c19d7013f51f6b821fd265
Jai Govindani <jai@honestbank.com>
2024-02-04 15:38:53 +0700

    feat(google_service_account): Add validation for account_id

--- terraform-gcp-iam-wrapper.tf
+++ terraform-gcp-iam-wrapper.tf
@@ -1,6 +1,20 @@
 terraform {
   required_version = "~> 1.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.14.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
+
+
 
 provider "google" {
   region      = var.google_region
